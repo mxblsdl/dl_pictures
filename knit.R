6b84fadee6a6f83b792fb5407fda4b685bca744c
mxblsdl <maxblasdel@gmail.com>
2021-04-17 11:50:57 -0700

    update to run on mac

--- knit.R
+++ knit.R
@@ -2,6 +2,9 @@
 library(rmarkdown)
 
 # to change
-setwd("/home/max/Documents/raspberry/dl_pictures/")
+setwd("/home/max/dl_pictures/")
 
 render("markdown_dl.Rmd", output_file = Sys.Date(), clean = T, output_dir = "reports")
+
+
+# firefox -kiosk --private-window http://167.71.158.245:3838/weather_clock/
